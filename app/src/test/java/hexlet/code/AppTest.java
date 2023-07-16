785c6d639fd258be421091b333de12ff0cfa0cb0
Pavel Vasilyev <pavel.vas@mail.ru>
2023-07-16 18:42:48 +0300

    Add first test

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+
+class AppTest {
+
+    private static Javalin app;
+    private static String baseUrl;
+
+    @BeforeAll
+    public static void beforeAll() {
+        app = App.getApp();
+        app.start();
+        baseUrl = "http://localhost:" + 8080;
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
+    }
+
+    @Test
+    void testRoot() {
+        HttpResponse<String> response = Unirest.get(baseUrl).asString();
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+}
