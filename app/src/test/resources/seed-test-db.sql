b0bb6ccb57c7c475ca68fae277efbf7446c525cb
Pavel Vasilyev <pavel.vas@mail.ru>
2023-07-24 22:04:40 +0300

    Fix test resources

--- app/src/test/resources/seed-test-db.sql
+++ app/src/test/resources/seed-test-db.sql
@@ -7,4 +7,4 @@ VALUES
   INSERT INTO url_check
     (status_code, title, h1, description, url_id, created_at)
   VALUES
-    (200, "Яндекс", "Все сервисы", "", 2, '2023-07-23 12:01:00')
+    (200, 'Яндекс', 'Все сервисы', '', 2, '2023-07-23 12:01:00');
