90b1436cc7f85900f0ba41daaf73c8099effb552
Pavel Vasilyev <pavel.vas@mail.ru>
2023-07-25 20:57:47 +0300

    Add UrlCheck tests

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,10 +1,14 @@
 package hexlet.code;
 
 import hexlet.code.domain.Url;
+import hexlet.code.domain.UrlCheck;
 import hexlet.code.domain.query.QUrl;
+import hexlet.code.domain.query.QUrlCheck;
 import io.ebean.DB;
 import io.ebean.Database;
 import io.javalin.Javalin;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,24 +18,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 class AppTest {
 
     private static Javalin app;
     private static String baseUrl;
     private static Database database;
+    private static MockWebServer mockServer;
+    private static final String TEST_PAGE_PATH = "./src/test/resources/fixtures/index.html";
 
     @BeforeAll
-    public static void beforeAll() {
+    public static void beforeAll() throws IOException {
         app = App.getApp();
         app.start();
         int port = app.port();
         baseUrl = "http://localhost:" + port;
         database = DB.getDefault();
+
+        Path filePath = Paths.get(TEST_PAGE_PATH).toAbsolutePath().normalize();
+        String body = Files.readString(filePath).trim();
+        mockServer = new MockWebServer();
+        mockServer.enqueue(new MockResponse().setBody(body));
+        mockServer.start();
     }
 
     @AfterAll
-    public static void afterAll() {
+    public static void afterAll() throws IOException {
         app.stop();
+        mockServer.shutdown();
     }
 
     @BeforeEach
@@ -142,5 +160,60 @@ class AppTest {
             String body = response.getBody();
             assertThat(response.getStatus()).isEqualTo(404);
         }
+    }
+
+    @Test
+    void testUrlCheck() {
+        String mockServerUrl = mockServer.url("/").toString().replaceAll("/$", "");
+
+        Unirest.post(baseUrl + "/urls")
+                .field("url", mockServerUrl)
+                .asString();
+
+        Url url = new QUrl()
+                .name.equalTo(mockServerUrl)
+                .findOne();
+
+        HttpResponse<String> responsePost = Unirest
+                .post(baseUrl + "/urls/" + url.getId() + "/checks")
+                .field("url", mockServerUrl)
+                .asString();
+
+        assertThat(responsePost.getStatus()).isEqualTo(302);
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls/" + url.getId())
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        UrlCheck urlcheck = new QUrlCheck()
+                .url.equalTo(url)
+                .orderBy()
+                .createdAt.desc()
+                .findOne();
+
+        assertThat(urlcheck).isNotNull();
+        assertThat(urlcheck.getStatusCode()).isEqualTo(200);
+        assertThat(urlcheck.getTitle()).isEqualTo("Test page");
+        assertThat(urlcheck.getH1()).isEqualTo("Test header");
+        assertThat(urlcheck.getDescription()).contains("This page was created for tests.");
+    }
+
+    @Test
+    void testUrlCheckFail() {
+        HttpResponse<String> responsePost = Unirest
+                .post(baseUrl + "/urls/2/checks")
+                .field("url", "https://github.commm")
+                .asString();
+
+        assertThat(responsePost.getStatus()).isEqualTo(302);
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls/2")
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getBody().contains("Exception"));
     }
 }
--- app/src/test/resources/seed-test-db.sql
+++ app/src/test/resources/seed-test-db.sql
@@ -2,9 +2,10 @@ INSERT INTO url
   (name, created_at)
 VALUES
   ('https://github.com', '2023-07-23 11:00:00'),
+  ('https://github.commm', '2023-07-23 11:00:00'),
   ('https://ya.ru', '2023-07-23 12:00:00');
 
   INSERT INTO url_check
     (status_code, title, h1, description, url_id, created_at)
   VALUES
-    (200, 'Яндекс', 'Все сервисы', '', 2, '2023-07-23 12:01:00');
+    (200, 'Яндекс', 'Все сервисы', '', 3, '2023-07-23 12:01:00');
